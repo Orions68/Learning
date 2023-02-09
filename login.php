eb24574bddbdcb11479de24fd5a3312e4698d87a
César Osvaldo Matelat Borneo <orions68@gmail.com>
2023-02-09 09:56:15 +0000

    Fixing login.php

--- login.php
+++ login.php
@@ -1,27 +1,27 @@
 <?php
-require("inc/fw.php");
-include_once 'header.php';
+include "inc/fw.php";
+include 'header.php';
 if (isset($_SESSION["name"]))
 {
-	echo "Ya has iniciado sesión.";
-	echo "Deseas cerrar sesión?";
-	echo "<a href='exit.php'>Salir</a>";
-	echo "<a href='inside.php'>Modificar la base de datos.</a>";
+	echo "<h1>Ya has iniciado sesión.</h1>
+	<h3>Deseas cerrar sesión?</h3>
+    <a href='exit.php'>Salir</a>
+    <br><br>
+	<a href='inside.php'>Modificar la base de datos.</a>";
 }
 else
 {
 ?>
 <h3>Por favor introduzca sus credenciales.</h3>
 <form action="inside.php" method="post">
-<p>
-<div>Nombre: </div><div class="align"><input type="text" name="name" /></div></p>
-<p>
-<div>Contraseña: </div><div class="align"><input type="password" name="pass" /></div>
-<input type="submit" value="Enviar" />
-</p>
+<div class="align"><label><input type="text" name="name"> Nombre</label></div>
+<br><br>
+<div class="align"><label><input type="password" name="pass"> Contraseña:</label></div>
+<br><br>
+<input type="submit" value="Enviar">
 </form>
 <?php
 mysqli_close($idconn);
 }
-include_once 'footer.php';
+include 'footer.php';
 ?>
