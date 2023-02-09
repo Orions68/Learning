ff0a9d39d49d4620fd5c9249f5e292a9ee5a2e7b
César Osvaldo Matelat Borneo <orions68@gmail.com>
2023-02-09 09:46:51 +0000

    Modifyng to neww code

--- add.php
+++ add.php
@@ -1,9 +1,9 @@
 <?php
-require("inc/fw.php");
-include_once 'header.php';
+include "inc/fw.php";
+include 'header.php';
 function add($name, $surname, $phone, $email)
 {
-	$sql = "insert into clientes (name, surname, phone, email) values ('$name', '$surname', '$phone', '$email')";
+	$sql = "INSERT INTO clientes (name, surname, phone, email) values ('$name', '$surname', '$phone', '$email')";
 	$result = mysqli_query($idconn, $sql);
 	if ($result === false)
 	{
@@ -11,12 +11,14 @@ function add($name, $surname, $phone, $email)
 	}
 	else
 	{
-		echo "Datos agregados correctamente.";
-		echo "<BR>Quieres hacer más modificaciones o cerrar sesión?";
-		echo "<form action=where.php method=POST><input type=submit name=modify value='Seguir editando' />";
-		echo "<input type=submit name=exit value='Salir y cerrar sesión' /></form>";
+		echo "Datos agregados correctamente.
+		<br><h3>Quieres hacer más modificaciones o cerrar sesión?</h3>
+		<form action='where.php' method='post'>
+        <input type='submit' name='modify' value='Seguir editando'>
+        <br><br>
+		<input type='submit' name='exit' value='Salir y cerrar sesión'></form>";
 	}
 }
 add($_POST['name'], $_POST['surname'], $_POST['phone'], $_POST['email']);
-include_once 'footer.php';
+include 'footer.php';
 ?>
